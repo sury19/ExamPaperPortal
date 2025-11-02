1eabe5f68b6bee701403a39ba5f3e990779f4327
suryaansh001 <sharmadev0042@gmail.com>
2025-11-02 12:28:12 +0530

    Fix CORS by disabling credentials

--- main.py
+++ main.py
@@ -544,7 +544,7 @@ app = FastAPI(title="Paper Portal API", version="2.0.0")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
